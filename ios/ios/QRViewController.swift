230e848590cb72e09c7815932bd1a3c3e3bd79e7
Jisssuu <wkdwltn1015@naver.com>
2023-01-25 11:04:46 +0900

    add QRView & QRViewController

---
+++ ios/ios/QRViewController.swift
@@ -0,0 +1,18 @@
+//
+//  QRViewController.swift
+//  ios
+//
+//  Created by 장지수 on 2023/01/25.
+//
+
+import UIKit
+
+class QRViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
