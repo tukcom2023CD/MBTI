41bb73e1f925a89eafc3b875d6ab0274654b72ec
rlaxoals36 <gimtaemin36@gmail.com>
2023-01-27 15:01:52 +0900

    setting: backend

---
+++ MBTI-backend/src/main/java/tukorea/MBTIbackend/MbtiBackendApplication.java
@@ -0,0 +1,13 @@
+package tukorea.MBTIbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MbtiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MbtiBackendApplication.class, args);
+	}
+
+}
---
+++ MBTI-backend/src/test/java/tukorea/MBTIbackend/MbtiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package tukorea.MBTIbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MbtiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
