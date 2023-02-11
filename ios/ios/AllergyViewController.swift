0a140617312716dd9bf4ec7ac0e4e601fa601b78
antonio26548 <antonio36548@tukorea.ac.kr>
2023-02-11 22:11:16 +0900

    FEAT: navigation view ,메인 화면으로 돌아가는 버튼 , allergyViewController 파일 추가

---
+++ ios/ios/AllergyViewController.swift
@@ -0,0 +1,17 @@
+//
+//  AllergyViewController.swift
+//  ios
+//
+//  Created by 박세인 on 2023/02/10.
+//
+
+import UIKit
+
+class AllergyViewController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+}
