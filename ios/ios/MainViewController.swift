21c5efe89683d6e579ad75ecaf5ade035972a531
Jisssuu <wkdwltn1015@naver.com>
2023-02-17 13:20:43 +0900

    SFSpeech 초기 설정

--- ios/ios/MainViewController.swift
+++ ios/ios/MainViewController.swift
@@ -6,9 +6,13 @@
 // test
 
 import UIKit
+import Speech
 
-class MainViewController: UIViewController {
+class MainViewController: UIViewController,SFSpeechRecognizerDelegate {
 
+    
+    @IBOutlet weak var SpeechView: UILabel!
+    @IBOutlet weak var SpeechButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
