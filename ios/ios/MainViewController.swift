db4e270165f93838516b40716679c0fd69632c2d
Jisssuu <wkdwltn1015@naver.com>
2023-02-19 15:03:53 +0900

    음성 인식을 위해 SFSpeechRecoziger 초기 설정 및 버튼 동작 구현

--- ios/ios/MainViewController.swift
+++ ios/ios/MainViewController.swift
@@ -14,9 +14,26 @@ class MainViewController: UIViewController,SFSpeechRecognizerDelegate {
     @IBOutlet weak var SpeechView: UILabel!
     @IBOutlet weak var SpeechButton: UIButton!
     
+    private let speechRecognizer = SFSpeechRecognizer(locale: Locale.init(identifier: "ko-KR"))
+    private var recognitionRequest: SFSpeechAudioBufferRecognitionRequest?
+    private var recognitionTask: SFSpeechRecognitionTask?
+    private let audioEngine = AVAudioEngine()
+    
+    @IBAction func speechToText(_ sender: Any) {
+        if audioEngine.isRunning {
+                audioEngine.stop()
+                recognitionRequest?.endAudio()
+                SpeechButton.isEnabled = false
+                SpeechButton.setTitle("Start Recording", for: .normal)
+            } else {
+                SpeechButton.setTitle("Stop Recording", for: .normal)
+            }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        speechRecognizer?.delegate = self
         // Do any additional setup after loading the view.
     }
+    
 }
 
