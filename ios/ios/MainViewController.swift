3274866877ffe0b46ca32219b3e9ff8535022cfc
Jisssuu <wkdwltn1015@naver.com>
2023-02-19 15:31:21 +0900

    버튼을 이용하여 STT 구현 및 Text에 결과물 도출

--- ios/ios/MainViewController.swift
+++ ios/ios/MainViewController.swift
@@ -28,7 +28,7 @@ class MainViewController: UIViewController,SFSpeechRecognizerDelegate {
             speechButton.isEnabled = false
             speechButton.setTitle("Start Recording", for: .normal)
         } else {
-            //startRecording()
+            startRecording()
             speechButton.setTitle("Stop Recording", for: .normal)
         }
     }
@@ -37,6 +37,68 @@ class MainViewController: UIViewController,SFSpeechRecognizerDelegate {
         speechRecognizer?.delegate = self
         // Do any additional setup after loading the view.
     }
-    
+    func startRecording() {
+        
+        if recognitionTask != nil {
+            recognitionTask?.cancel()
+            recognitionTask = nil
+        }
+        
+        let audioSession = AVAudioSession.sharedInstance()
+        do {
+            try audioSession.setCategory(AVAudioSession.Category.record)
+            try audioSession.setMode(AVAudioSession.Mode.measurement)
+            try audioSession.setActive(true, options: .notifyOthersOnDeactivation)
+        } catch {
+            print("audioSession properties weren't set because of an error.")
+        }
+        
+        recognitionRequest = SFSpeechAudioBufferRecognitionRequest()
+        
+        let inputNode = audioEngine.inputNode
+        
+        guard let recognitionRequest = recognitionRequest else {
+            fatalError("Unable to create an SFSpeechAudioBufferRecognitionRequest object")
+        }
+        
+        recognitionRequest.shouldReportPartialResults = true
+        
+        recognitionTask = speechRecognizer?.recognitionTask(with: recognitionRequest, resultHandler: { (result, error) in
+            
+            var isFinal = false
+            
+            if result != nil {
+                
+                self.speechText.text = result?.bestTranscription.formattedString
+                isFinal = (result?.isFinal)!
+            }
+            
+            if error != nil || isFinal {
+                self.audioEngine.stop()
+                inputNode.removeTap(onBus: 0)
+                
+                self.recognitionRequest = nil
+                self.recognitionTask = nil
+                
+                self.speechButton.isEnabled = true
+            }
+        })
+        
+        let recordingFormat = inputNode.outputFormat(forBus: 0)
+        inputNode.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) { (buffer, when) in
+            self.recognitionRequest?.append(buffer)
+        }
+        
+        audioEngine.prepare()
+        
+        do {
+            try audioEngine.start()
+        } catch {
+            print("audioEngine couldn't start because of an error.")
+        }
+        
+        speechText.text = "Say something, I'm listening!"
+        
+    }
 }
 
