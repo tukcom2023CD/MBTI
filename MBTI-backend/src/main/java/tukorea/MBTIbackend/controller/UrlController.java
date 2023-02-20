56ace0194601748e6faaea3ee1bac7f337e7a784
rlaxoals36 <gimtaemin36@gmail.com>
2023-02-20 19:29:56 +0900

    feat: UrlController

--- MBTI-backend/src/main/java/tukorea/MBTIbackend/controller/UrlController.java
+++ MBTI-backend/src/main/java/tukorea/MBTIbackend/controller/UrlController.java
@@ -1,16 +1,20 @@
 package tukorea.MBTIbackend.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.jsoup.select.Elements;
 import org.springframework.web.bind.annotation.*;
 import tukorea.MBTIbackend.domain.Url;
+import tukorea.MBTIbackend.service.ProductDataService;
+
+import java.io.IOException;
 
 @Slf4j
 @RestController
 public class UrlController {
 
     @PostMapping("/url")
-    public String Url(@RequestBody Url url) {
-        log.info("url = {}", url.getUrl());
-        return "ddd";
+    public Elements Url(@ModelAttribute Url url) throws IOException {
+     //   log.info("url = {}", url.getUrl());
+        return ProductDataService.getProductDatas(url);
     }
 }
