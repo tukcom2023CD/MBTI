b3c8591e1bb3eb1c73c9477f00ea45bc875d0d8e
rlaxoals36 <gimtaemin36@gmail.com>
2023-02-22 13:23:13 +0900

    feat: ProductDto

---
+++ MBTI-backend/src/main/java/tukorea/MBTIbackend/comparison/dto/ProductDto.java
@@ -0,0 +1,12 @@
+package tukorea.MBTIbackend.comparison.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ProductDto {
+
+    private int id;
+    private String allergy;
+}
