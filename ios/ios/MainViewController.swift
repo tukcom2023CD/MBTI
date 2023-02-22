6b85e8e00474990735ac66c80e55fe56f39a2f4c
Jisssuu <wkdwltn1015@naver.com>
2023-02-22 13:32:10 +0900

    TTS에서 음성이 출력이 되지 않는 버그 해결

--- ios/ios/MainViewController.swift
+++ ios/ios/MainViewController.swift
@@ -11,7 +11,7 @@ import AVFoundation
 
 class MainViewController: UIViewController,SFSpeechRecognizerDelegate {
 
-    
+    let synthesizer = AVSpeechSynthesizer()
     @IBOutlet weak var speechButton: UIButton!
     @IBOutlet weak var speechText: UITextView!
     var text : String = ""
@@ -129,7 +129,6 @@ class MainViewController: UIViewController,SFSpeechRecognizerDelegate {
         
     }
     func textToSpeech(_ errorText:String) {
-        let synthesizer = AVSpeechSynthesizer()
         let utterance = AVSpeechUtterance(string: errorText)
         utterance.voice = AVSpeechSynthesisVoice(language:"ko-KR")
         utterance.rate = 0.4
