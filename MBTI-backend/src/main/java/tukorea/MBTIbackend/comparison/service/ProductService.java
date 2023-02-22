3fbdf219e95d5a421dc56d109b950c89b9cb1f2f
rlaxoals36 <gimtaemin36@gmail.com>
2023-02-22 13:23:53 +0900

    feat: ProductService

---
+++ MBTI-backend/src/main/java/tukorea/MBTIbackend/comparison/service/ProductService.java
@@ -0,0 +1,10 @@
+package tukorea.MBTIbackend.comparison.service;
+
+import tukorea.MBTIbackend.comparison.dto.ProductDto;
+
+import java.util.List;
+
+public interface ProductService {
+
+    public List<ProductDto> getProductList();
+}
