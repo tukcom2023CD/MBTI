8c00025df761bcca2a38489deaa33d74cf718869
Jisssuu <wkdwltn1015@naver.com>
2023-03-07 17:43:18 +0900

    Realm 연동을 위한 데이터베이스 형태 파일

---
+++ ios/ios/Product.swift
@@ -0,0 +1,19 @@
+//
+//  Product.swift
+//  ios
+//
+//  Created by 장지수 on 2023/03/07.
+//
+
+import Foundation
+import RealmSwift
+
+class Product: Object {
+    @objc dynamic var prdno:Int = 0
+    @objc dynamic var allergy:String = ""
+    @objc dynamic var num:Int = 0
+    
+    override static func primaryKey() -> String? {
+         return "num"
+       }
+}
