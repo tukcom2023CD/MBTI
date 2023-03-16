d7ff93eb7a7606d7eb3910c4c92600379d2d3578
BAESlee <lees13579@tukorea.ac.kr>
2023-03-16 18:45:58 +0900

    db 삭제 (impoort 제거)

--- MBTI-backend/src/main/java/tukorea/MBTIbackend/crawling/controller/UrlController.java
+++ MBTI-backend/src/main/java/tukorea/MBTIbackend/crawling/controller/UrlController.java
@@ -3,9 +3,7 @@ package tukorea.MBTIbackend.crawling.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.jsoup.select.Elements;
 import org.springframework.web.bind.annotation.*;
-import tukorea.MBTIbackend.comparison.service.ProductService;
 import tukorea.MBTIbackend.crawling.domain.Url;
-import tukorea.MBTIbackend.crawling.service.ProductDataService;
 
 import java.io.IOException;
 
