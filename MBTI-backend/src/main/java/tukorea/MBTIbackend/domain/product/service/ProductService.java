2cb509ef6cd2f67647e7352b39876acb3d4b6eb6
rlaxoals36 <gimtaemin36@gmail.com>
2023-04-04 16:31:04 +0900

    feat : productId로 db조회 후 Optional처리

--- MBTI-backend/src/main/java/tukorea/MBTIbackend/domain/product/service/ProductService.java
+++ MBTI-backend/src/main/java/tukorea/MBTIbackend/domain/product/service/ProductService.java
@@ -6,6 +6,12 @@ import org.springframework.transaction.annotation.Transactional;
 import tukorea.MBTIbackend.domain.product.dto.ProductResponseDto;
 import tukorea.MBTIbackend.domain.product.entity.Product;
 import tukorea.MBTIbackend.domain.product.repository.ProductRepository;
+import tukorea.MBTIbackend.crawling.service.ProductDataService;
+
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+import static tukorea.MBTIbackend.crawling.service.ProductDataService.getProductDatas;
 
 @AllArgsConstructor
 @Service
@@ -14,8 +20,25 @@ public class ProductService {
 
     @Transactional
     public ProductResponseDto findProductByPID(Long productId) {
-        Product product = productRepository.findProductAllergyByProductId(productId).orElseThrow();
+       // Product product = productRepository.findProductAllergyByProductId(productId).orElseThrow();
+       // return new ProductResponseDto(product);
 
-        return new ProductResponseDto(product);
+        Optional<Product> optionalProduct = productRepository.findProductAllergyByProductId(productId);
+        try {
+            if (optionalProduct.isPresent()) {
+                Product product = optionalProduct.get();
+                return new ProductResponseDto(product);
+                // do something with the product
+            } else {
+               // throw new NoSuchElementException("Product not found with ID: " + productId);
+                ProductDataService productDataService = new ProductDataService();
+               // Product product = getProductDatas(productId.toString());
+
+            }
+        } catch (NoSuchElementException e) {
+            // handle the case where the product is not found
+            System.out.println(e.getMessage());
+        }
+        return null;
     }
 }
