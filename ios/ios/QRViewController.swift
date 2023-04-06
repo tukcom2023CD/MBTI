4f81c0c478dabb6be6f17d74c796da6f1c8cf949
antonio26548 <antonio36548@tukorea.ac.kr>
2023-04-06 18:04:14 +0900

    [Test] QRCresult 화면이동 기능 수정

--- ios/ios/QRViewController.swift
+++ ios/ios/QRViewController.swift
@@ -18,7 +18,7 @@ class QRViewController: UIViewController {
     let realm = try! Realm()
     var timeTrigger = true
     var realTime = Timer()
-    @IBAction func moveresult(_ sender: Any){
+    func moveresult(){
         let resultview = self.storyboard?.instantiateViewController(withIdentifier: "QRCresult")
         resultview?.modalTransitionStyle = UIModalTransitionStyle.coverVertical
         self.present(resultview!, animated: true, completion: nil)
@@ -155,8 +155,8 @@ extension QRViewController: AVCaptureMetadataOutputObjectsDelegate {
 
             // qr코드가 가진 문자열이 URL 형태를 띈다면 출력.(아무런 qr코드나 찍는다고 출력시키면 안되니까 여기서 분기처리 가능. )
             if stringValue.hasPrefix("http://www.foodqr.kr") || stringValue.hasPrefix("https://www.foodqr.kr/foodqr?")  {
-                UIApplication.shared.open(URL(string:stringValue)!,options: [:])
-                moveresult(<#Any#>)
+//                UIApplication.shared.open(URL(string:stringValue)!,options: [:])
+                moveresult()
                 stopAction()
                 let startIndex = stringValue.index(stringValue.startIndex,offsetBy: 35)
                 let range = startIndex...
