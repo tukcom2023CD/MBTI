4c71edcdf8c83ee4be1347e7a13d736e3b4b2397
rlaxoals36 <gimtaemin36@gmail.com>
2023-04-09 17:26:34 +0900

    feat : productname 크롤링 후 save

--- MBTI-backend/src/main/java/tukorea/MBTIbackend/domain/product/service/ProductService.java
+++ MBTI-backend/src/main/java/tukorea/MBTIbackend/domain/product/service/ProductService.java
@@ -7,21 +7,19 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import lombok.AllArgsConstructor;
+import lombok.SneakyThrows;
+import tukorea.MBTIbackend.domain.product.entity.Product;
+import tukorea.MBTIbackend.domain.product.repository.ProductRepository;
 import tukorea.MBTIbackend.domain.product.dto.ProductResponseDto;
 
 import java.io.IOException;
-import java.util.SimpleTimeZone;
+import java.util.NoSuchElementException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import lombok.AllArgsConstructor;
-import lombok.SneakyThrows;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-import tukorea.MBTIbackend.domain.product.dto.ProductResponseDto;
-import tukorea.MBTIbackend.domain.product.entity.Product;
-import tukorea.MBTIbackend.domain.product.repository.ProductRepository;
 import java.util.Optional;
 
 @AllArgsConstructor
@@ -35,49 +33,68 @@ public class ProductService {
 
         Optional<Product> optionalProduct = productRepository.findProductAllergyByProductId(productId);
 
-        if (optionalProduct.isPresent()) {
-            Product product = optionalProduct.get();
-            return new ProductResponseDto(product);
-        }
-        else
-            return new ProductResponseDto(getProductDatas(productId));
-    }
+            if (optionalProduct.isPresent()) {
+                Product product = optionalProduct.get();
+                return new ProductResponseDto(product);
+            } else
+                return new ProductResponseDto(getProductDatas(productId));
 
+    }
 
     private Product getProductDatas(Long productId) throws IOException, ParseException {
 
         String PRODUCT_DATA_URL = "http://www.foodqr.kr/foodqr?PRD_NO=";
-
         String pre_allergen = null;
+        String prdlst_nm = null;
+
         Document doc = Jsoup.connect(PRODUCT_DATA_URL+productId).get();
         Elements scriptElements = doc.getElementsByTag("script");
 
         for (Element element : scriptElements) {
             if (element.data().contains("foodLab")) {
-                Pattern pattern = Pattern.compile("\"PRI_ALLERGEN\":.*함유\"");
-                Matcher matcher = pattern.matcher(element.data());
 
-                if (matcher.find()) {
-                    pre_allergen = matcher.group(0);
-                    break;
+                Pattern pattern_allergen = Pattern.compile("\"PRI_ALLERGEN\":.*함유\"");
+                Matcher matcher_allergen = pattern_allergen.matcher(element.data());
+
+                if (matcher_allergen.find()) {
+                    pre_allergen = matcher_allergen.group(0);
                 } else {
                     System.err.println("No match found!");
                 }
+
+                Pattern pattern_prdlst = Pattern.compile("\"PRDLST_NM\":\"위러브플러스\",\\s*\"P[^\"]*\"");
+                Matcher matcher_prdlst = pattern_prdlst.matcher(element.data());
+
+                if (matcher_prdlst.find()) {
+                    prdlst_nm = matcher_prdlst.group(0);
+                } else {
+                    System.err.println("No math found!");
+                }
+
                 break;
             }
         }
 
         JSONParser parser = new JSONParser();
         JSONObject jsonObject = (JSONObject) parser.parse("{"+pre_allergen+"}");
+
+        int pnlength = prdlst_nm.length();
+        StringBuilder pnsb = new StringBuilder(prdlst_nm);
+        pnsb.delete(pnlength - 13, pnlength);
+        String jsonproductname = pnsb.toString();
+        JSONObject pnjsonObject = (JSONObject) parser.parse("{"+jsonproductname+"}");
+        String productname = (String)pnjsonObject.get("PRDLST_NM");
+
         String allergy = (String)jsonObject.get("PRI_ALLERGEN");
-        int length = allergy.length();
-        StringBuilder sb = new StringBuilder(allergy);
-        sb.delete(length - 2, length);
-        String allergen = sb.toString();
+        int palength = allergy.length();
+        StringBuilder pasb = new StringBuilder(allergy);
+        pasb.delete(palength - 3, palength);
+        String allergen = pasb.toString();
 
         Product product = Product.builder()
                 .productId(productId)
                 .allergy(allergen)
+                .productname(productname)
                 .build();
 
         return productRepository.save(product);
